a46fcdfe0c204309be251173cc395ae22ccabf98
Ajzner, Yeshayahu Dov <YeshayahuDov.Ajzner@ca.com>
2017-12-07 11:41:25 +0200

    adding some tests 1

---
+++ run_api_tests.sh
@@ -0,0 +1,1 @@
+python -m pytest --capture=no
---
+++ test_hello.py
@@ -0,0 +1,10 @@
+import mathlib
+
+def test_tota1():
+    res = mathlib.calc_total(2, 3)
+    assert res==5
+
+
+def test_tota2():
+    res = mathlib.calc_total(2, 3)
+    assert res==6
