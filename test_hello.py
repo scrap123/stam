6f3b587c00511190dcfe71b6e73bfe9305917661
Ajzner, Yeshayahu Dov <YeshayahuDov.Ajzner@ca.com>
2017-12-07 14:03:10 +0200

    ttrttttttttt

--- test_hello.py
+++ test_hello.py
@@ -1,15 +1,21 @@
 import mathlib
 
-def test_tota1():
+
+def test_total1():
     res = mathlib.calc_total(2, 3)
     assert res==5
 
 
-def test_tota2():
+def test_total2():
     res = mathlib.calc_total(2, 3)
     assert res==5
 
 
 def test_total3():
     res = mathlib.calc_total(2, 10)
+    assert res==12
+
+
+def test_total4():
+    res = mathlib.calc_total(2, 130)
     assert res==12
